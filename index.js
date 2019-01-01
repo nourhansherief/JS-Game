2f66ba100aeb85e82d3713257ea001496963a0e6
nourhansherief <45966082+nourhansherief@users.noreply.github.com>
2019-01-01 17:43:01 +0200

    Add files via upload
    feat:Building a Paddle
    
    Starting the game with building a paddle using html 5 canvas and java script . Make a game loop to handle paddle across the screen which will be handled later with input.

---
+++ index.js
@@ -0,0 +1,46 @@
+//import * as Paddle from "/src/paddle";
+
+//import paddle from 'paddle';
+
+let canvas = document.getElementById("myCanvas");
+
+let ctx = canvas.getContext("2d");
+console.log(ctx);
+
+const GAME_WIDTH = 800;
+
+const GAME_HEIGHT = 600;
+
+
+ctx.clearRect(0, 0, 800, 600);
+
+// Create gradient
+var grd = ctx.createRadialGradient(75, 50, 5, 90, 60, 100);
+grd.addColorStop(0, "red");
+grd.addColorStop(1, "white");
+
+// Fill with gradient
+//ctx.fillStyle = grd;
+//ctx.fillRect(10,10,150,80);
+
+// Fill with gradient
+ctx.fillStyle = '#00f';
+//ctx.fillRect(300,300,70,60);
+
+
+let paddle = new Paddle(GAME_WIDTH, GAME_HEIGHT);
+
+paddle.draw(ctx);
+
+let lastTime = 0;
+
+function gameLoop(timestamp) {
+    let deltaTime = timestamp - lastTime;
+    lastTime = timestamp;
+    ctx.clearRect(0, 0, 800, 600);
+    paddle.update(deltaTime);
+    paddle.draw(ctx);
+
+    requestAnimationFrame(gameLoop);
+}
+gameLoop();
