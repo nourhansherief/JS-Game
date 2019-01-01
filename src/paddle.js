9b16032d25021e6936e2046aab199b15a3ce7d7f
saratarek224 <saratarek224@hotmail.com>
2019-01-01 20:52:13 +0200

    Add files via upload

--- src/paddle.js
+++ src/paddle.js
@@ -1,7 +1,11 @@
  class Paddle {
     constructor(gameWidth, gameHeight) {
+        this.gameWidth=gameWidth;
         this.width = 150;
-        this.height = 30;
+        this.height = 20;
+
+        this.maxSpeed =7;
+        this.speed=0;
 
         this.position = {
             x: gameWidth / 2 - this.width / 2,
@@ -9,6 +13,19 @@
         }
     }
 
+
+    moveLeft(){
+        this.speed= -this.maxSpeed;
+    }
+
+    moveRight(){
+
+        this.speed=this.maxSpeed;
+    }
+
+    stop(){
+        this.speed=0;
+    }
 
     draw(ctx) {
         ctx.fillStyle='#0ff';
@@ -18,7 +35,14 @@
 
     update(deltaTime)
     {
-        if(!deltaTime) return;
+
+        this.position.x += this.speed;
+        if(this.position.x <0){
+            this.position.x=0;
+        }
+        if(this.position.x+this.width > this.gameWidth){
+            this.position.x=this.gameWidth-this.width;
+        }
         //this.position.x +=5/deltaTime;
     }
 }
