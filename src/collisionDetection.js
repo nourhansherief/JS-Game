1494d90a0c4d107a61ccaabd7d75a20006acd08d
fatemaMO <45967448+fatemaMO@users.noreply.github.com>
2019-01-02 19:55:06 +0200

    Add files via upload

--- src/collisionDetection.js
+++ src/collisionDetection.js
@@ -1,24 +1,21 @@
-
-
-function detectCollision ( ball, gameObject) 
+function detectCollision(ball, gameObject) 
 {
-    let bottomOfBall = ball.postion.y + ball.size;
-    let topOfBall = ball.postion.y;
-  
-    let topOfObject = gameObject.postion.y;
-    let leftSideOfObject = gameObject.postion.x;
-    let rightSideOfObject = gameObject.postion.x + gameObject.width;
-    let bottomOfObject = gameObject.postiony + gameObject.height;
-  
-    if (
-      bottomOfBall >= topOfObject &&
-      topOfBall <= bottomOfObject &&
-      ball.postion.x >= leftSideOfObject &&
-      ball.postion.x + ball.size <= rightSideOfObject
-    ) {
-      return true;
-    } 
-    else {
-      return false;
-    }
+  let bottomOfBall = ball.postion.y + ball.size;
+  let topOfBall = ball.postion.y;
+
+  let topOfObject = gameObject.postion.y;
+  let leftSideOfObject = gameObject.postion.x;
+  let rightSideOfObject = gameObject.postion.x + gameObject.width;
+  let bottomOfObject = gameObject.postion.y + gameObject.height;
+
+  if (
+    bottomOfBall >= topOfObject &&
+    topOfBall <= bottomOfObject &&
+    ball.postion.x >= leftSideOfObject &&
+    ball.postion.x + ball.size <= rightSideOfObject
+  ) {
+    return true;
+  } else {
+    return false;
   }
+}
